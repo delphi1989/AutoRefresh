bfb490fbd257c5e0e8da64ffd4472009ebeadfa0
wangziyingwen <wz.lxh@outlook.com>
2020-03-09 01:14:07 +0800

    把 commit 合併成一個

--- 1.py
+++ 1.py
@@ -1,4 +1,5 @@
 # -*- coding: UTF-8 -*-
+import os
 import requests as req
 import json,sys,time
 #先注册azure应用,确保应用有以下权限:
@@ -7,24 +8,20 @@ import json,sys,time
 #mail:  Mail.Read、Mail.ReadWrite、MailboxSettings.Read、MailboxSettings.ReadWrite
 #注册后一定要再点代表xxx授予管理员同意,否则outlook api无法调用
 
-
-
-
+id = os.environ["CONFIG_ID"]
+secret = os.environ["CONFIG_KEY"]
 
 
 path=sys.path[0]+r'/1.txt'
-path2=sys.path[0]+r'/time.log'
 num1 = 0
 
 def gettoken(refresh_token):
-    headers={'Content-Type':'application/x-www-form-urlencoded'
-            }
+    headers={'Content-Type':'application/x-www-form-urlencoded'}
     data={'grant_type': 'refresh_token',
           'refresh_token': refresh_token,
           'client_id':id,
           'client_secret':secret,
-          'redirect_uri':'http://localhost:53682/'
-         }
+          'redirect_uri':'http://localhost:8000/signin-oidc'}
     html = req.post('https://login.microsoftonline.com/common/oauth2/v2.0/token',data=data,headers=headers)
     jsontxt = json.loads(html.text)
     refresh_token = jsontxt['refresh_token']
@@ -35,10 +32,7 @@ def gettoken(refresh_token):
 def main():
     fo = open(path, "r+")
     refresh_token = fo.read()
-    fo.close() 
-    fv = open(path2, "r+")
-    timelog = fv.read()
-    fv.close()
+    fo.close()
     global num1
     localtime = time.asctime( time.localtime(time.time()) )
     access_token=gettoken(refresh_token)
@@ -61,11 +55,11 @@ def main():
             print('4调用成功'+str(num1)+'次')
         if req.get(r'https://graph.microsoft.com/v1.0/me/messages',headers=headers).status_code == 200:
             num1+=1
-            print('5调用成功'+str(num1)+'次')    
+            print('5调用成功'+str(num1)+'次')
         if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',headers=headers).status_code == 200:
             num1+=1
-            print('6调用成功'+str(num1)+'次')    
-        if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',headers=headers).status_code == 200:
+            print('6调用成功'+str(num1)+'次')
+        if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders/Inbox/messages/delta',headers=headers).status_code == 200:
             num1+=1
             print('7调用成功'+str(num1)+'次')
         if req.get(r'https://graph.microsoft.com/v1.0/me/drive/root/children',headers=headers).status_code == 200:
@@ -73,14 +67,14 @@ def main():
             print('8调用成功'+str(num1)+'次')
         if req.get(r'https://api.powerbi.com/v1.0/myorg/apps',headers=headers).status_code == 200:
             num1+=1
-            print('8调用成功'+str(num1)+'次') 
+            print('8调用成功'+str(num1)+'次')
         if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders',headers=headers).status_code == 200:
             num1+=1
             print('9调用成功'+str(num1)+'次')
         if req.get(r'https://graph.microsoft.com/v1.0/me/outlook/masterCategories',headers=headers).status_code == 200:
             num1+=1
             print('10调用成功'+str(num1)+'次')
-            print('此次运行时间为 :', localtime)
+            print('此次运行结束时间为 :', localtime)
     except:
         print("pass")
         pass
